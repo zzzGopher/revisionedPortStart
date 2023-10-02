e33a403940afb1ec5f72579b17510cdc8bd16604
zzzGopher <gophermac90@yahoo.com>
2023-10-01 22:56:45 -0500

    Implemented work section

--- Pages/MockDbExample.cshtml.cs
+++ Pages/MockDbExample.cshtml.cs
@@ -1,36 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using uTestAndForms.Data;
 using uTestAndForms.Models;
 
 namespace uTestAndForms.Pages;
 
 public class IndexModel : PageModel
 {
+    public IEmployeeRespository _employeeRepo;
 
 
     public ILogger<IndexModel> _logger;
+
+
+    public IndexModel(ILogger<IndexModel> logger, IEmployeeRespository employeeRepo)
+    {
+        _logger = logger;
+        _employeeRepo = employeeRepo;
+    }
 
 
     [Required(ErrorMessage = "please select a gendyyy")]
 
     public string gender { get; set; }
 
-    [BindProperty]
-    public int _id {
-        get;
-        set;
-    }
-
-    public IEmployeeRespository _employeeRepo;
-
-
-    public IndexModel(ILogger<IndexModel> logger,IEmployeeRespository employeeRepo)
-    {
-        _logger = logger;
-        _employeeRepo = employeeRepo;
-    }
+    [BindProperty] public int _id { get; set; }
 
     public IActionResult OnGet()
     {
@@ -41,20 +35,19 @@ public class IndexModel : PageModel
     {
         _employeeRepo.Add(myUsers);
         _logger.LogInformation("posted");
-        return Redirect("/Index");
+        return Redirect("/MockDbExample");
     }
 
     public IActionResult OnPostDeleteUsers(int ID)
     {
         _logger.LogInformation("Deleting");
         _employeeRepo.ClearUser(ID);
-        return Redirect("/Index");
+        return Redirect("/MockDbExample");
     }
 
     public IActionResult OnPostDeleteAllUsers()
     {
         _employeeRepo.ClearAllUsers();
-        return Redirect("/Index");
+        return Redirect("/MockDbExample");
     }
-
 }
